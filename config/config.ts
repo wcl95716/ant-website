8badde8015b0e8cd017a96e92685c7ab3b0139fe
panda <347038217@qq.com>
2024-05-15 03:07:38 +0800

    feat: update

--- config/config.ts
+++ config/config.ts
@@ -158,8 +158,8 @@ export default defineConfig({
             localIdentName: '[local]_[hash:base64:5]',
         },
     },
-    // https: {
-    //     cert: '/root/.acme.sh/panda-website.top_ecc/panda-website.top.cer',
-    //     key: '/root/.acme.sh/panda-website.top_ecc/panda-website.top.key',
-    // },
+    https: {
+        cert: '/root/.acme.sh/panda-website.top_ecc/panda-website.top.cer',
+        key: '/root/.acme.sh/panda-website.top_ecc/panda-website.top.key',
+    },
 });
